2f8e157b8ba7114ee92598679cb870f5716d9294
Chris Dawson <chrisdawson1996@gmail.com>
2024-09-01 12:37:42 +0100

    remove old files

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,4 @@
-pub mod locations {
-    pub mod config;
-    pub mod events;
-    pub mod location;
-    pub mod plugin;
-    pub mod setup;
-}
 pub mod game_plugin;
-pub mod input;
-pub mod ui {
-    pub mod encounter;
-    pub mod game_over;
-    pub mod home;
-    pub mod plugin;
-    pub mod resources;
-}
-pub mod events;
 pub mod overworld {
     pub mod berry_bush;
     pub mod collisions;
